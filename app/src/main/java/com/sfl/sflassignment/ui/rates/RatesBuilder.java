029797a8f67009028d4bb8d8f93837c73b97626e
zigkurat <vahan@zeroapp.ai>
2018-10-18 23:14:18 +0400

    removed comments

--- app/src/main/java/com/sfl/sflassignment/ui/rates/RatesBuilder.java
+++ app/src/main/java/com/sfl/sflassignment/ui/rates/RatesBuilder.java
@@ -5,24 +5,6 @@ import com.sfl.sflassignment.ui.rates.presenter.RatesPresenter;
 import com.sfl.sflassignment.ui.rates.view.RatesActivity;
 
 public class RatesBuilder {
-//    wireframe = BatchFilingWireframe()
-//
-//    let presenter = BatchFilingPresenter()
-//    presenter.wireframe = wireframe
-//    wireframe.presenter = presenter
-//
-//    let interactor = BatchFilingInteractor(account: account)
-//    presenter.interactor = interactor
-//    interactor.output = presenter
-//    interactor.dmsAccountManager = BatchFilingDMSAccountManager()
-//    interactor.aiEngineWrapper = BatchFilingAIEWrapper()
-//    interactor.pendingOpsWrapper = BatchFilingPendingOpsWrapper()
-//
-//    let loadingViewController = BatchFilingViewController()
-//    loadingViewController.wireframe = wireframe
-//    presenter.userInterface = loadingViewController
-//    loadingViewController.eventHandler = presenter
-//    wireframe.initialViewController = loadingViewController
 
     public void build(RatesActivity view) {
         RatesPresenter presenter = new RatesPresenter();
